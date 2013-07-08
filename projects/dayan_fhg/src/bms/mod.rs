5dcbfbc71a6233e99e005eaa54dec1d34f50b19e
Aster <aster@vers.site>
2013-07-08 03:34:48 +0000

    🎀 Custom BMS expand steps

--- projects/dayan_fhg/src/bms/mod.rs
+++ projects/dayan_fhg/src/bms/mod.rs
@@ -1,83 +1,80 @@
-use std::fmt;
+use std::{
+    fmt::{Display, Formatter},
+    num::NonZeroUsize,
+};
 
-struct Bms {
-    s: Vec<Vec<usize>>,
-    b: Option<usize>,
+#[derive(Clone)]
+struct BashicuMatrixSystem {
+    matrix: Vec<Vec<usize>>,
+    expand: NonZeroUsize,
 }
 
-impl Bms {
-    fn new(s: Vec<Vec<usize>>, b: Option<usize>) -> Bms {
-        Bms { s, b }
+impl BashicuMatrixSystem {
+    pub fn new(s: Vec<Vec<usize>>) -> BashicuMatrixSystem {
+        BashicuMatrixSystem { matrix: s, expand: unsafe { NonZeroUsize::new_unchecked(2) } }
+    }
+    pub fn with_expand_steps(mut self, steps: NonZeroUsize) -> BashicuMatrixSystem {
+        self.expand = steps;
+        self
     }
 
-    fn expand(&self) -> Bms {
-        let s = &self.s;
+    fn expand(&self) -> BashicuMatrixSystem {
+        let s = &self.matrix;
         let xs = self.xs();
         let ys = self.ys();
-
-        let s1 = self.s[..xs - 1].to_vec();
-        println!("s1: {:?}", s1);
-
+        let s1 = self.matrix[..xs - 1].to_vec();
         let r = match self.get_bad_root() {
             Some(r) => r,
-            None => return Bms::new(s1, None),
+            None => return BashicuMatrixSystem::new(s1).with_expand_steps(self.expand),
         };
-
-        let mut delta = sub(&s[xs - 1], &s[r as usize]);
+        let mut delta = diff(&s[xs - 1], &s[r]);
         let lmnz = match self.get_lowermost_nonzero(&s[xs - 1]) {
             Some(s) => s,
-            None => return Bms::new(s1, None),
+            None => return BashicuMatrixSystem::new(s1).with_expand_steps(self.expand),
         };
-
         for y in lmnz..ys {
             delta[y] = 0;
         }
-
         let a = self.get_ascension();
-        let bs = xs - r as usize - 1;
-
+        let bs = xs - r - 1;
         let mut s1 = s1;
-        for i in 0..self.b.unwrap() {
+        for i in 0..self.expand.get() {
             for x in 0..bs {
                 let mut da = vec![0; ys];
                 for y in 0..ys {
-                    da[y] = s[r as usize + x][y] + delta[y] * a[x][y] * (i + 1);
+                    da[y] = s[r + x][y] + delta[y] * a[x][y] * (i + 1);
                 }
                 s1.push(da);
             }
         }
-
-        Bms::new(s1, None)
+        BashicuMatrixSystem::new(s1).with_expand_steps(self.expand)
     }
 
-    fn get_parent(&self, x: usize, y: usize) -> i32 {
-        let mut p = x as i32;
+    fn get_parent(&self, x: usize, y: usize) -> Option<usize> {
+        let mut p = x;
         while p > 0 {
             if y != 0 {
-                p = self.get_parent(p as usize, y - 1);
+                p = self.get_parent(p, y - 1)?;
             }
+            // else if p == 0 {
+            //     return None;
+            // }
             else {
-                p -= 1;
+                p -= 1
             }
-            if p == -1 {
-                return p;
-            }
-            if self.s[p as usize][y] < self.s[x][y] {
-                return p;
+            if self.matrix[p][y] < self.matrix[x][y] {
+                return Some(p);
             }
         }
-        -1
+        None
     }
 
     fn get_bad_root(&self) -> Option<usize> {
         let xs = self.xs();
         let x = xs - 1;
-        let y = self.get_lowermost_nonzero(&self.s[x])?;
-        let p = self.get_parent(x, y);
-        if p == -1 {
-            return None;
-        }
-        Some(p as usize)
+        let y = self.get_lowermost_nonzero(&self.matrix[x])?;
+        let p = self.get_parent(x, y)?;
+        Some(p)
     }
 
     fn get_ascension(&self) -> Vec<Vec<usize>> {
@@ -87,21 +84,21 @@ impl Bms {
             Some(r) => r,
             None => return vec![],
         };
-        let bs = xs - r as usize - 1;
+        let bs = xs - r - 1;
         let mut a = vec![vec![0; ys]; bs];
         for y in 0..ys {
             a[0][y] = 1;
             for x in 1..bs {
-                let p = self.get_parent(x + r as usize, y);
-                if p == -1 {
+                let p = match self.get_parent(x + r, y) {
+                    Some(s) => s,
+                    None => continue,
+                };
+                // FIXME: Strange case
+                // println!("p: {}, r: {}", p, r);
+                if p < r {
                     continue;
                 }
-
-                println!("p: {}, r: {}", p, r);
-                if p < r as i32 {
-                    continue;
-                }
-                if p != -1 && a[(p as usize - r)][y] == 1 {
+                if a[p - r][y] == 1 {
                     a[x][y] = 1;
                 }
             }
@@ -119,18 +116,18 @@ impl Bms {
     }
 
     fn xs(&self) -> usize {
-        self.s.len()
+        self.matrix.len()
     }
 
     fn ys(&self) -> usize {
-        self.s[0].len()
+        self.matrix[0].len()
     }
 }
 
-impl fmt::Display for Bms {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl Display for BashicuMatrixSystem {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut result = String::new();
-        for c in &self.s {
+        for c in &self.matrix {
             result.push('(');
             for (i, &r) in c.iter().enumerate() {
                 result.push_str(&r.to_string());
@@ -140,14 +137,12 @@ impl fmt::Display for Bms {
             }
             result.push(')');
         }
-        if let Some(b) = self.b {
-            result.push_str(&format!("[{}]", b));
-        }
+        result.push_str(&format!("[{}]", self.expand));
         write!(f, "{}", result)
     }
 }
 
-fn sub(a: &[usize], b: &[usize]) -> Vec<usize> {
+fn diff(a: &[usize], b: &[usize]) -> Vec<usize> {
     a.iter().zip(b.iter()).map(|(&x, &y)| x - y).collect()
 }
 
@@ -156,8 +151,11 @@ fn test() {
     //  (0,0,0)(1,1,1)(2,1,0)(1,1,0)(2,2,1)(3,1,0)(2,2,1)
     let sequence =
         vec![vec![0, 0, 0], vec![1, 1, 1], vec![2, 1, 0], vec![1, 1, 0], vec![2, 2, 1], vec![3, 1, 0], vec![2, 2, 1]];
-    let b = Some(2);
-    let bms = Bms::new(sequence, b);
+    let bms = BashicuMatrixSystem::new(sequence.clone());
+    println!("{}", bms);
+    let bms = BashicuMatrixSystem::new(sequence.clone()).expand();
+    println!("{}", bms);
+    let bms = bms.expand();
     println!("{}", bms);
     let bms = bms.expand();
     println!("{}", bms);
